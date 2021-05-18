b944c833934938edeb48703cdc09960e623efe07
nasiegel88 <nasiegel@ucdavis.edu>
2021-05-18 08:49:07 -0700

    fix backup

--- backup.sh
+++ backup.sh
@@ -1,4 +1,2 @@
 #!bin/bash
-cp -r /share/workshop/mca_workshop/nasiegel ~/
-mkdir -p 2021-mca_workshop && cp -r  ~/nasiegel/* 2021-mca_workshop/ 
-mv --force  2021-mca_workshop/*  ~/2021-mca_workshop/
+cp -r /share/workshop/mca_workshop/nasiegel/* ~/2021-mca_workshop/
