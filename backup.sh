8b62c1b5969073e1a3f0d99076e932a11b42718c
nasiegel88 <nasiegel@ucdavis.edu>
2021-05-18 08:32:03 -0700

    first commit!

---
+++ backup.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+cp -r /share/workshop/mca_workshop/nasiegel ~/
+mkdir -p 2021-mca_workshop && cp -r  ~/nasiegel/* 2021-mca_workshop/ 
+mv --force  2021-mca_workshop/*  ~/2021-mca_workshop/
